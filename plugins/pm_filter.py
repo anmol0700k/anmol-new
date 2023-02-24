436c1b8498f09af5b2a2686829efacd87562e0f1
anmol0700k <117492721+anmol0700k@users.noreply.github.com>
2023-02-24 09:55:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -658,6 +658,7 @@ async def auto_filter(client, msg, spoll=False):
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
                 await client.send_message(req_channel, f"#REQUESTED_LOGS \n\n**CONTENT NAME:**'{search}' \n**REQUESTED BY:** {message.from_user.first_name}\n**USER ID:** {message.from_user.id}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✅ Mark as Done ✅", callback_data="close_data")]]))
+                await message.reply_text(text=f"Hello '{messsage.from_user.first_name}' 😊. \n\n Your request has been sent to your admin ! \n\n ❤️ THANK YOU ❤️ <a herf='https://t.me/Request_Log'>Channel Name</a>")
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
                 else:
